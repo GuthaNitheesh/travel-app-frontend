2fb0d82c77ba495834b5206dd8ae1196042cc39e
Gutha Nitheesh <guthanitheesh@gmail.com>
2025-07-28 21:28:44 +0530

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
-import { Home ,Payment,SearchResults,SingleHotel, Wishlist,OrderSummary} from './pages';
+import { Route, Routes } from 'react-router-dom';
+import { Home, Payment, SearchResults, SingleHotel, Wishlist, OrderSummary } from './pages';
+import { useAuth } from './context';
+import { AuthModal } from './components';
 
 function App() {
+  const { isAuthModalOpen } = useAuth(); // ✅ import state
+
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/hotels/:name/:address/:id' element={<SingleHotel/>}></Route>
-      <Route path='/hotels/:address' element={<SearchResults/>}/>
-      <Route path='/wishlist' element={<Wishlist></Wishlist>}></Route>
-      <Route path='/confirm-booking/stay/:id' element={<Payment></Payment>}></Route>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels/:name/:address/:id' element={<SingleHotel />} />
+        <Route path='/hotels/:address' element={<SearchResults />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/confirm-booking/stay/:id' element={<Payment />} />
         <Route path="/order-summary" element={<OrderSummary />} />
-    </Routes>
-    
+      </Routes>
+
+      {/* ✅ Global AuthModal */}
+      {isAuthModalOpen && <AuthModal />}
+    </>
   );
 }
 
